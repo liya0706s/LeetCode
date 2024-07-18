f2326dd33d3f8c272d9bd9dcbeeee821936d978e
李亞璇(Angie.Lee)-TW <01440498@sftw.com>
2024-07-18 14:03:47 +0800

    20240718_414_third max havent take a note

--- s20240718_414_thirdMax.php
+++ s20240718_414_thirdMax.php
@@ -1,10 +1,15 @@
 <?php
-
-
-
-
-
-
+class Solution{
+    function thirdMax($nums){
+        $nums=array_unique($nums);
+        if(count($nums)<3){
+            return max($nums);
+        }else{
+            rsort($nums);
+            return $nums[2];
+        }
+    }
+}
 
 
 ?>
