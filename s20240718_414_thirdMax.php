bc21816492ceda611e40b392934ebfe2ae5bec2a
李亞璇(Angie.Lee)-TW <01440498@sftw.com>
2024-07-29 15:57:51 +0800

    thirdMax add notes

--- s20240718_414_thirdMax.php
+++ s20240718_414_thirdMax.php
@@ -1,11 +1,18 @@
 <?php
 class Solution{
+    // 定義一個函數 thirdMax，接受一個數組 $nums 作為參數
     function thirdMax($nums){
+        // 使用 array_unique 去除數組中的重複值
         $nums=array_unique($nums);
+
+        // 如果數組的元素個數少於 3 個
         if(count($nums)<3){
+            // 返回數組中的最大值
             return max($nums);
         }else{
+            // 將數組按降序排序
             rsort($nums);
+            // 返回排序後的數組中的第三個元素
             return $nums[2];
         }
     }
